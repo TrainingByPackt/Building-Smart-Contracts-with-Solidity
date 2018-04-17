301a0da9ff6b97f2bf48e1679fd90a39fcd2e3f5
gangachris <ganga.chris@gmail.com>
2018-04-17 03:06:47 +0300

    mytoken sample

---
+++ lesson1/topic2/mytoken.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.16;
+contract MyToken {
+    // tokens total supply
+    uint totalSupply;
+
+    // constructor: instantiate total supply
+    function MyToken(uint total) public {
+        totalSupply = total;
+    }
+
+    // mint additional tokens
+    function mint(uint n) public {
+        totalSupply = totalSupply + n;
+    }
+
+    // get the current total tokens
+    function get () public constant returns (uint) {
+        return totalSupply;
+    }
+}
